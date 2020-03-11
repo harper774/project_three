83c290e478383116e6ec4c08d7b100b97c7319fd
Harper <harperliulove@gmail.com>
2020-03-11 20:17:39 +1100

    set up basic course

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Courses from "./pages/Courses";
+// import Nav from "./components/Nav";
+
+function App() {
+    return (
+        <div>
+            <Courses />
+        </div>
+    );
+}
+
+export default App;
