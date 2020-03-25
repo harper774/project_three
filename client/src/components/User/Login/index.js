15ae3da0e6ea5ef5d690041bdcd9a41a077904d8
Harper <harperliulove@gmail.com>
2020-03-25 20:17:01 +1100

    user

---
+++ client/src/components/User/Login/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Login() {
+    return (
+        <form id="test-form" class="white-popup-block mfp-hide">
+            <div class="popup_box ">
+                <div class="popup_inner">
+                    <div class="logo text-center">
+                        <a href="#">
+                            <img src="img/form-logo.png" alt=""></img>
+                        </a>
+                    </div>
+                    <h3>Sign in</h3>
+                    <form action="#">
+                        <div class="row">
+                            <div class="col-xl-12 col-md-12">
+                                <input type="email" placeholder="Enter email"></input>
+                            </div>
+                            <div class="col-xl-12 col-md-12">
+                                <input type="password" placeholder="Password"></input>
+                            </div>
+                            <div class="col-xl-12">
+                                <button type="submit" class="boxed_btn_orange">Sign in</button>
+                            </div>
+                        </div>
+                    </form>
+                    <p class="doen_have_acc">Don’t have an account? <a class="dont-hav-acc" href="#test-form2">Sign Up</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default Login;
