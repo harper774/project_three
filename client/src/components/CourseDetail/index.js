e74663f2917cf27502109349f6fe0bb48870ad97
Harper <harperliulove@gmail.com>
2020-03-25 20:16:04 +1100

    course detail

---
+++ client/src/components/CourseDetail/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./Banner";
+import Info from "./Info";
+
+function CourseDetail() {
+    return (
+        <div>
+            <Banner />
+            <Info />
+        </div>
+    );
+}
+
+export default CourseDetail;
