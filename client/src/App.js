54da47e51c5cc4d4f9cfcd475fd2bb035ecfe9b0
Harper <harperliulove@gmail.com>
2020-03-25 20:15:04 +1100

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Courses from "./pages/Courses";
 // import Nav from "./components/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+
     return (
-        <div>
-            <Courses />
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" component={Courses}/>
+                
+            </Switch>
+        </Router>
     );
 }
 
