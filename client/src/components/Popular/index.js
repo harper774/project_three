9b7e8bac56513d724c037c1323fb7ef2379b5540
Harper <harperliulove@gmail.com>
2020-03-25 20:16:33 +1100

    popular

--- client/src/components/Popular/index.js
+++ client/src/components/Popular/index.js
@@ -1,9 +1,33 @@
 import React from "react";
-import "./style.scss";
+import Header from "./Header";
+import Nav from "./Nav";
+import Course from "./Course";
+import More from "./More";
+
 
 function Popular() {
-    return(
-        <div>This is where the popular courses are.</div>
+    return (
+        <div>
+            <div className="popular_courses">
+                <div className="container">
+                    <Header />
+                    <Nav />
+                </div>
+
+                <div className="all_courses">
+                    <div class="container">
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <div class="row">
+                                    <Course />
+                                    <More />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
