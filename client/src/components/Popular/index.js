225d1bb052a7ec528d8963f4b99a2b09c6d7b2bc
Harper <harperliulove@gmail.com>
2020-03-30 16:03:11 +1100

    updatew popular component

--- client/src/components/Popular/index.js
+++ client/src/components/Popular/index.js
@@ -4,23 +4,33 @@ import Nav from "./Nav";
 import Course from "./Course";
 import More from "./More";
 
+const style = {
+    marginTop: 90,
+    // backgroundImage: "linear-gradient(to top, #e6b980 0%, #eacda3 100%)",
+    repeat: "no-repeat",
+    color: "white"
+}
 
 function Popular() {
     return (
         <div>
-            <div className="popular_courses">
-                <div className="container">
-                    <Header />
-                    <Nav />
-                </div>
+            <div className="all_popular_courses" style={style}>
+                <div className="container mt-5">
+                    <div className="popular_courses">
+                        <div className="container">
+                            <Header />
+                            <Nav />
+                        </div>
 
-                <div className="all_courses">
-                    <div class="container">
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                <div class="row">
-                                    <Course />
-                                    <More />
+                        <div className="all_courses">
+                            <div class="container">
+                                <div class="tab-content" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <Course />
+                                            <More />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
