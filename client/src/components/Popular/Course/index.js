6cb164c0491220af67c3600ead5de8bdeebe021d
Harper <harperliulove@gmail.com>
2020-03-30 16:03:19 +1100

    update course in popular

--- client/src/components/Popular/Course/index.js
+++ client/src/components/Popular/Course/index.js
@@ -7,22 +7,22 @@ function Course() {
             <div class="single_courses">
                 <div class="thumb">
                     <a href="#">
-                        <img src="img/courses/1.png" alt=""></img>
+                        <img src="https://www.getitrightbookkeeping.com/images/figures/desk.jpg" alt=""></img>
                     </a>
                 </div>
                 <div class="courses_info">
-                    <span>Photoshop</span>
-                    <h3><a href="#">Mobile App design step by step <br></br>
-                        from beginner</a></h3>
+                    <span>Javascript</span>
+                    <h3><a href="#">Music Search App Dev <br></br>
+                        for beginner</a></h3>
                     <div class="star_prise d-flex justify-content-between">
                         <div class="star">
                             <i class="flaticon-mark-as-favorite-star"></i>
                             <span>(4.5)</span>
                         </div>
                         <div class="prise">
-                            <span class="offer">$89.00</span>
+                            {/* <span class="offer">$89.00</span> */}
                             <span class="active_prise">
-                                $49
+                                Free
                         </span>
                         </div>
                     </div>
