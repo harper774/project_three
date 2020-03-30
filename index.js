9fa39887e6196cd9fa6383f8a7fa282fad793728
Harper <harperliulove@gmail.com>
2020-03-30 18:38:30 +1100

    update index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const session = require("express-session");
+const db = require("./models");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3003;
+const passport = require("./config/passport");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +17,21 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+// Express Session
+app.use(session({
+    secret: "secret code",
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/onlinecourse");
 
-// Start the API server
-app.listen(PORT, function () {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// Run the server
+db.sequelize.authenticate()
+// eslint-disable-next-line no-console
+.then(() => app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`)));
